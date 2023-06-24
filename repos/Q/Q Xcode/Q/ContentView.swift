adb624be90f28d2e0b486fbfbb79908df1d49cce
Jacob L <jacob@Hannahs-Air-2.fios-router.home>
2023-06-24 11:06:08 -0400

    Fix issue where info.plist is showing macOS properties instead of iOS. Re-create xcode Project file and transfer files from old Project file to new file.

---
+++ repos/Q/Q Xcode/Q/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Q
+//
+//  Created by Jacob L on 6/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Create.")
+                    .font(.title)
+                    .foregroundColor(Color.green)
+                    .multilineTextAlignment(.center)
+                
+                Text("Find your LGBTQ+ friendly community.")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                
+                NavigationLink(destination: SignUpController()) {
+                    Text("Create Account")
+                }
+                
+                NavigationLink(destination:
+                                Login()) {
+                    Text("Login")
+                }
+                .padding()
+            }
+        }
+    }
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
+
